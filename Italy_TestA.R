8c01c4f635f5373ab16cccc7d05fdce4206acc2b
Asim-Dey <100403847+Asim-Dey@users.noreply.github.com>
2023-03-13 23:07:17 -0600

    Update Italy_TestA.R

--- Italy_TestA.R
+++ Italy_TestA.R
@@ -7,17 +7,11 @@ library(NetSwan)
 
 
 
-data11 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/Export_Output22.csv")
-data11 <- read.csv("C:/Users/adey/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/Export_Output22.csv")
-
-
-source('C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
-source('C:/Users/adey/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
+data11 <- read.csv("Export_Output22.csv")
+source('Pref_at_G.R')
 
 
 nodes_Italy<-data11[,c(2,3,4)][data11[,5]=="Italy",]
-
-#write.csv(nodes_Italy,"C:/Users/akd130230/Dropbox/Power Grid Network/Data from author/processed/Italy_nodes.csv")
 
 ID_Italy<-data11[,2][data11[,5]=="Italy"]
 
@@ -27,8 +21,6 @@ ID_Italy<-data11[,2][data11[,5]=="Italy"]
 #Italy<-data11[(data11[,17] %in% ID_Italy) & (data11[,18] %in% ID_Italy)  , ]
 
 Italy<-data11[(data11[,17] %in% ID_Italy) | (data11[,18] %in% ID_Italy)  , ] # or
-#write.csv(Italy,"C:/Users/akd130230/Dropbox/Power Grid Network/Data from author/processed/Italy_edge.csv")
-
 Italy_data_all<-Italy[,c(16,17,18)]
 
 
@@ -130,8 +122,7 @@ M0<-c(m1[4],m2[8],m2[9],m2[10]);M0
 #################################### eNN ################################################
 
 
-G2 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Test_Italy_eNN_Motifs.txt", header=FALSE)[1:2000,]
-G2 <- read.table("C:/Users/adey/OneDrive/Synthentic Network/Random Graph Model/Data/Test_Italy_eNN_Motifs.txt", header=FALSE)[1:2000,]
+G2 <- read.table("Test_Italy_eNN_Motifs.txt", header=FALSE)[1:2000,]
 
 names(G2)<-c('nodes','edges','Diameter','AVPL','triangles','square','k13','tent','kite','k4')
 
@@ -307,16 +298,6 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
-
-
-#     Stat   Observed   Mean         s      Zm     LL    UL
-#     AVPL    9.971   6.67714  0.2879699 11.439 -1.796 2.545
-# Diameter   28.000  14.52400  1.3846959  9.732 -1.101 1.788
-#       T2   23.000 161.29100 14.3869319 -9.612 -2.244 2.065
-#       V3  133.000 765.88400 92.2398267 -6.861 -1.809 2.365
-#       V4   29.000  35.39850  7.5172437 -0.851 -1.649 2.075
-#       V5    6.000 121.89800 19.6512514 -5.898 -1.878 1.990
-
 
 
 
@@ -325,8 +306,7 @@ data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=
 #################################### CLC 1 param ################################################
 
 
-G2 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Test_Italy_1parar_Motifs.txt", header=FALSE)[1:2000,]
-G2 <- read.table("C:/Users/adey/OneDrive/Synthentic Network/Random Graph Model/Data/Test_Italy_1parar_Motifs.txt", header=FALSE)[1:2000,]
+G2 <- read.table("Test_Italy_1parar_Motifs.txt", header=FALSE)[1:2000,]
 
 names(G2)<-c('nodes','edges','Diameter','AVPL','triangles','square','k13','tent','kite','k4')
 
@@ -502,16 +482,6 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
-
-
-
-#     stat    Observed  Mean        s       Zm     LL    UL
-#     AVPL    9.971  11.26477  0.8997255 -1.438 -1.761 1.384
-# Diameter   28.000  29.58550  4.1532294 -0.382 -2.067 1.544
-#       T2   23.000  32.33600  6.4879234 -1.439 -1.747 1.798
-#       V3  133.000 151.72050 27.5371300 -0.680 -1.842 1.971
-#       V4   29.000  20.13150  4.4712642  1.983 -1.819 1.760
-#       V5    6.000  12.59650  5.1657555 -1.277 -1.471 1.820
 
 
 
@@ -523,8 +493,7 @@ data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=
 #################################### CLC 2 param ################################################
 
 
-G2 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Test_Italy_2parar_Motifs.txt", header=FALSE)
-G2 <- read.table("C:/Users/adey/OneDrive/Synthentic Network/Random Graph Model/Data/Test_Italy_2parar_Motifs.txt", header=FALSE)
+G2 <- read.table("Test_Italy_2parar_Motifs.txt", header=FALSE)
 
 names(G2)<-c('nodes','edges','Diameter','AVPL','triangles','square','k13','tent','kite','k4')
 
@@ -701,39 +670,19 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
 
-#      Stat Observed  Mean      s        Zm     LL    UL
-#     AVPL    9.971  9.82726  0.7987042 0.180 -1.524 1.706
-# Diameter   28.000 25.13450  3.7044934 0.774 -1.656 1.853
-#       T2   23.000 16.30250  3.9308675 1.704 -1.858 1.958
-#       V3  133.000 74.48700 18.3034789 3.197 -1.884 2.159
-#       V4   29.000 12.95350  4.0354033 3.976 -1.723 2.242
-#       V5    6.000  3.71800  2.3061082 0.990 -1.612 2.290
-
 
 
 
 #######################################################################################
 #################################### GeoDe ################################################
 
-GG1 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Italy_6000C.txt", header=FALSE)
-GG2 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Italy_6000D.txt", header=FALSE)
-GG3 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Italy_6000D1.txt", header=FALSE)
-
-
-
-
-GG1 <- read.csv("C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/Data/Italy_6000C.txt", header=FALSE)
-GG2 <- read.csv("C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/Data/Italy_6000D.txt", header=FALSE)
-GG3 <- read.csv("C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/Data/Italy_6000D1.txt", header=FALSE)
-
-
-
+GG1 <- read.csv("Italy_6000C.txt", header=FALSE)
+GG2 <- read.csv("Italy_6000D.txt", header=FALSE)
+GG3 <- read.csv("Italy_6000D1.txt", header=FALSE)
 
 
 G2<-rbind(GG1,GG2,GG3)
 dim(G2)
-
-
 
 
 names(G2)<-c("m", "t","nodes", "edges", "vee", "triangles", "path3", "square", 
@@ -741,10 +690,6 @@ names(G2)<-c("m", "t","nodes", "edges", "vee", "triangles", "path3", "square",
 
 head(G2)
 dim(G2)
-
-#write.csv(dd,"C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/A/Data/ER_Italy.csv")
-
-
 
 #############################################################################################
 ####################################################################################################
@@ -913,25 +858,6 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
-
-
-
-#    Stat Observed      Mean          s     Zm     LL    UL
-#     AVPL    9.971   9.19660  0.8107862  0.956 -1.535 2.290
-# Diameter   28.000  22.17533  3.1831871  1.830 -1.312 2.458
-#       T2   23.000  24.43200  5.7299736 -0.250 -1.821 2.193
-#       V3  133.000 122.94000 32.1840998  0.313 -1.786 2.115
-#       V4   29.000  17.98800  5.6183835  1.960 -1.778 2.138
-#       V5    6.000   5.68400  3.4337547  0.092 -1.364 2.131
-
-
-
-
-
-
-
-
-
 
 
 ################################# ER and PA ###############################################
@@ -1156,41 +1082,6 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
-
-
-
-
-############ ER #######################
-
-
-#   Stat Observed      Mean          s     Zm     LL    UL
-#     AVPL    9.971  5.385842  0.1339493 34.233 -1.871 2.004
-# Diameter   28.000 12.418000  1.1467055 13.588 -1.237 2.252
-#       T2   23.000  3.387000  1.8544407 10.576 -1.826 1.948
-#       V3  133.000 26.949000 15.8079203  6.709 -1.705 2.281
-#       V4   29.000  6.910000  2.7354417  8.075 -1.795 2.226
-#       V5    6.000  0.135500  0.4089295 14.341 -0.331 2.114
-
-
-
-
-############# PA ######################################
-
-#     Stat Observed      Mean          s     Zm     LL    UL
-#     AVPL    9.971   4.99613  0.1072302 46.397 -1.903 2.070
-# Diameter   28.000  11.72100  1.0003296 16.274 -1.720 2.278
-#       T2   23.000  14.01000  3.5643025  2.522 -1.967 2.242
-#       V3  133.000 285.69850 81.5475849 -1.873 -1.725 2.199
-#       V4   29.000  34.03450  7.2663409 -0.693 -1.794 2.060
-#       V5    6.000   8.01250  5.0062836 -0.402 -1.401 2.195  
-
-
-
-
-
-
-
-
 
 
 
