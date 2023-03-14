067461da1bed49ff2890e54942e054af9771e677
Asim-Dey <100403847+Asim-Dey@users.noreply.github.com>
2023-03-13 22:30:56 -0600

    Update CLC Italy.R

--- CLC Italy.R
+++ CLC Italy.R
@@ -7,17 +7,17 @@ library(NetSwan)
 
 
 
-data11 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/Export_Output22.csv")
-data11 <- read.csv("C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/Export_Output22.csv")
+data11 <- read.csv("Export_Output22.csv")
+data11 <- read.csv("Export_Output22.csv")
 
 
-source('C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
-source('C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
+source('Pref_at_G.R')
+source('Pref_at_G.R')
 
 
 nodes_Italy<-data11[,c(2,3,4)][data11[,5]=="Italy",]
 
-#write.csv(nodes_Italy,"C:/Users/akd130230/Dropbox/Power Grid Network/Data from author/processed/Italy_nodes.csv")
+#write.csv(nodes_Italy,"Italy_nodes.csv")
 
 ID_Italy<-data11[,2][data11[,5]=="Italy"]
 
@@ -26,7 +26,7 @@ ID_Italy<-data11[,2][data11[,5]=="Italy"]
 #Italy<-data11[(data11[,17] %in% ID_Italy) & (data11[,18] %in% ID_Italy)  , ]
 
 Italy<-data11[(data11[,17] %in% ID_Italy) | (data11[,18] %in% ID_Italy)  , ] # or
-#write.csv(Italy,"C:/Users/akd130230/Dropbox/Power Grid Network/Data from author/processed/Italy_edge.csv")
+#write.csv(Italy,"Italy_edge.csv")
 
 Italy_data_all<-Italy[,c(16,17,18)]
 
@@ -110,7 +110,7 @@ degree_Germany
 
 ####################################################################################################
 
-CLC10 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Python/CLC simulated data/N_Italy_10.txt",header=TRUE)
+CLC10 <- read.table("N_Italy_10.txt",header=TRUE)
 CLC10<-as.matrix(CLC10)+1 # few nodes have level 0; changing them t 1
 
 G1<-graph_from_edgelist(CLC10,directed = FALSE)
