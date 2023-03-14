35202f28eef84f51025dbf3767b36d90d86c198f
Asim-Dey <100403847+Asim-Dey@users.noreply.github.com>
2023-03-13 22:38:10 -0600

    Update IEEE118_TestA.R

--- IEEE118_TestA.R
+++ IEEE118_TestA.R
@@ -9,14 +9,12 @@ library(EnvStats)
 ##############################IEEE 300 Bus system ##############################
 
 
-Data_IEEE300<-read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/IEEE_118_Bus.csv",header=TRUE)
-Data_IEEE300<-read.csv("C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/IEEE_118_Bus.csv",header=TRUE)
+Data_IEEE300<-read.csv("IEEE_118_Bus.csv",header=TRUE)
+Data_IEEE300<-read.csv("IEEE_118_Bus.csv",header=TRUE)
 
 
 
-source('C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
-source('C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
-
+source('Pref_at_G.R')
 
 
 #-----Remove duplicity -----------------------------------------------------------
@@ -130,7 +128,7 @@ M0<-c(m1[4],m2[8],m2[9],m2[10]);M0
 #######################################################################################
 #################################### eNN ################################################
 
-G2 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Test_IEEE118_eNN_Motifs.txt", header=FALSE)
+G2 <- read.table("Test_IEEE118_eNN_Motifs.txt", header=FALSE)
 
 names(G2)<-c('nodes','edges','Diameter','AVPL','triangles','square','k13','tent','kite','k4')
 
@@ -307,23 +305,12 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
-
-#     Stat   Observed   Mean        s       Zm     LL    UL
-#     AVPL    6.309   5.10878  0.2752473  4.359 -1.703 2.257
-# Diameter   14.000  11.08900  1.1509741  2.529 -1.815 2.529
-#       T2   23.000  88.23450 13.4695666 -4.843 -1.799 2.136
-#       V3  132.000 445.52850 88.8356070 -3.529 -1.661 2.223
-#       V4   20.000  20.86500  6.8486661 -0.126 -1.586 2.210
-#       V5    5.000  78.92500 20.6111325 -3.587 -1.646 2.187
-
-
-
 
 
 #######################################################################################
 #################################### CLC 1 param ################################################
 
-G2 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Test_IEEE118_1parar_Motifs.txt", header=FALSE)[1:2000,]
+G2 <- read.table("Test_IEEE118_1parar_Motifs.txt", header=FALSE)[1:2000,]
 
 names(G2)<-c('nodes','edges','Diameter','AVPL','triangles','square','k13','tent','kite','k4')
 
@@ -500,23 +487,6 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
-
-
-#     Stat   Observed Mean          s        Zm     LL    UL
-#     AVPL    6.309  7.347615  0.5517621 -1.883 -1.409 1.472
-# Diameter   14.000 18.383500  2.5234127 -1.737 -1.737 1.829
-#       T2   23.000 11.017500  3.8839073  3.085 -1.807 1.798
-#       V3  132.000 47.909500 17.6484289  4.765 -1.695 2.215
-#       V4   20.000  9.969000  3.2025251  3.132 -1.552 2.203
-#       V5    5.000  2.563000  1.9819170  1.230 -1.293 2.239
-
-
-
-
-
-
-
-
 
 
 
@@ -526,7 +496,7 @@ data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=
 #######################################################################################
 #################################### CLC 2 param ################################################
 
-G2 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/Test_IEEE118_2parar_Motifs.txt", header=FALSE)
+G2 <- read.table("Test_IEEE118_2parar_Motifs.txt", header=FALSE)
 names(G2)<-c('nodes','edges','Diameter','AVPL','triangles','square','k13','tent','kite','k4')
 
 head(G2)
@@ -703,31 +673,13 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
 
-#      Stat Observed    Mean      s         Zm     LL    UL
-#     AVPL    6.309  5.961035  0.3419486  1.017 -1.816 2.044
-# Diameter   14.000 14.446000  1.6505897 -0.270 -1.482 2.153
-#       T2   23.000  9.107000  2.9971724  4.635 -1.704 1.966
-#       V3  132.000 42.897000 14.4159738  6.181 -1.796 2.019
-#       V4   20.000  6.078500  2.7533849  5.056 -1.481 2.151
-#       V5    5.000  2.646000  2.0767379  1.134 -1.274 2.578
-
-
-
-
-
-
-
-
-
-
 
 
 #######################################################################################
 #################################### GeoDe ################################################
 
-GG1 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/IEEE118_6000.txt", header=FALSE)
-GG2 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/IEEE118_6000B.txt", header=FALSE)
-#GG3 <- read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Data/IEEE118_6000C.txt", header=FALSE)
+GG1 <- read.csv("IEEE118_6000.txt", header=FALSE)
+GG2 <- read.csv("IEEE118_6000B.txt", header=FALSE)
 
 G2<-rbind(GG1,GG2)
 
@@ -909,14 +861,6 @@ UQA<-round(c(UQ_AVPL11,UQ_Dim11,UQ_T2,UQ_V3,UQ_V4,UQ_V5),3)
 
 data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=LQA,'UL'=UQA) 
 
-
-#      Stat Observed    Mean         s     Zm     LL    UL
-#     AVPL    6.309   5.943686  0.525153  0.695 -1.476 2.248
-# Diameter   14.000  13.723000  2.044593  0.135 -1.332 2.092
-#       T2   23.000  23.445000  5.939917 -0.075 -1.758 1.950
-#       V3  132.000 121.298000 35.019171  0.306 -1.693 2.219
-#       V4   20.000  15.821500  5.386478  0.776 -1.638 2.075
-#       V5    5.000   9.119500  4.915415 -0.838 -1.448 2.417
 
 
 
@@ -1144,50 +1088,6 @@ data.frame ('Stat'=Motif, 'Observed'=Ob_Occur, 'Mean'=Mean,'s'=sd1,'Zm'=Zm,'LL'=
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-############  ER ###########################
-
-  #      Stat Observed      Mean          s     Zm     LL    UL
-  #     AVPL    6.309   4.211095  0.1244334 16.857 -2.019 2.055
-  #  Diameter   14.000  9.418500  0.9585495  4.780 -1.480 2.693
-  #       T2   23.000  4.573000  2.2176857  8.309 -1.611 1.996
-  #       V3  132.000 38.205500 19.4780637  4.815 -1.602 2.248
-  #       V4   20.000  9.581000  3.4392373  3.029 -1.623 2.157
-  #       V5    5.000  0.520000  0.8584687  5.219 -0.606 2.889
-  
-  
-############  PA ##############
-
-
-  #     Stat Observed       Mean          s     Zm     LL    UL
-  #     AVPL    6.309   4.097325  0.1093167 20.229 -1.920 2.066
-  # Diameter   14.000   9.234000  0.8767145  5.436 -1.408 2.014
-  #       T2   23.000  11.464500  3.1663469  3.643 -1.726 2.064
-  #       V3  132.000 184.656500 57.4637150 -0.916 -1.752 2.095
-  #       V4   20.000  25.142000  6.3374220 -0.811 -1.758 2.187
-  #       V5    5.000   7.013500  4.5068252 -0.447 -1.334 2.438
-
-
-
-
-
-
-
-
-
 
 
 
