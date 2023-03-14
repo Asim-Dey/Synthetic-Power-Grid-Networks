59025e94f050bb4290cc2e2e7b3558517a8bb515
Asim-Dey <100403847+Asim-Dey@users.noreply.github.com>
2023-03-13 23:08:01 -0600

    Update Poisson_NN_118.R

--- Poisson_NN_118.R
+++ Poisson_NN_118.R
@@ -9,12 +9,8 @@ library(EnvStats)
 ##############################IEEE 300 Bus system ##############################
 
 
-Data_IEEE300<-read.csv("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/IEEE_118_Bus.csv",header=TRUE)
-#Data_IEEE300<-read.csv("C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Data/IEEE_118_Bus.csv",header=TRUE)
-
-
-source('C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
-source('C:/Users/akd130230/OneDrive/Synthentic Network/Random Graph Model/R codes/2020/Old/Pref_at_G.R')
+Data_IEEE300<-read.csv("IEEE_118_Bus.csv",header=TRUE)
+source('Pref_at_G.R')
 
 
 
@@ -118,8 +114,7 @@ V5<-m2[10];V5
 V6<-m2[11];V6
 
 c(n,m,Diameter_G,AVPL_G,m1[4],m2[8],m2[9],m2[10],m2[11])
-# 118.000000 179.000000  14.000000   6.308706  23.000000 132.000000  20.000000   5.000000
-#   1.000000
+
 
 c(T2,n,m)
 
@@ -140,7 +135,7 @@ degree_Germany<-degree_Germany_N*118 ;degree_Germany #Node 445
 
 ####################################################################################################
 
-CLC10 <- read.table("C:/Users/asimi/OneDrive/Synthentic Network/Random Graph Model/Python/Poisson simulated data/IEEE118_PoissonNN_10.txt",header=TRUE)
+CLC10 <- read.table("Poisson simulated data/IEEE118_PoissonNN_10.txt",header=TRUE)
 CLC10<-as.matrix(CLC10)+1 # few nodes have level 0; changing them t 1
 
 G1<-graph_from_edgelist(CLC10,directed = FALSE)
